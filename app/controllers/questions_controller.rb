c6ade2c069cfaed9915464092de352c31171d1d9
martin hoenle <martin.hoenle@icloud.com>
2023-05-26 03:04:04 +0200

    stupif coach complete

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,23 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+
+    @question = params[:question]
+
+    stop_message = "I am going to work right now!"
+
+    if @question == stop_message || @question == stop_message.upcase
+    @answer = ""
+    elsif @question.end_with?("?")
+    @answer = "Silly question, get dressed and go to work!"
+    else
+    @answer = "I don't care, get dressed and go to work!"
+
+    end
+
+  end
+
+end
